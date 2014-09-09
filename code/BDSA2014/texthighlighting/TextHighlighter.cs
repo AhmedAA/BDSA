5b2d5fe4dd007824b0191940fa7c326955d074ba
phillipphoelich <pphoelich@gmail.com>
2014-09-09 13:41:58 +0200

    My code moved around, so it's not in the "phillip" folder any longer.

---
+++ code/BDSA2014/texthighlighting/TextHighlighter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BDSA13;
+
+namespace texthighlighting
+{
+    /// <summary>
+    /// Assignment 37 - Text highlighting.
+    /// </summary>
+    class TextHighlighter
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public TextHighlighter()
+        {
+            
+        }
+
+        public void HightlightText(string input, string searchTerm)
+        {
+            
+        }
+
+    }
+}
