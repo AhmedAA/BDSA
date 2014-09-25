c8ef051ab446945040ad944218e4db12a7afe108
Mikkelaaxman <mikkelaaxman@gmail.com>
2014-09-25 16:54:35 +0200

    Færdig RPC_V2

---
+++ code/BDSA2014/reversepolishcalculator/IOperation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reversepolishcalculator
+{
+    interface IOperation
+    {
+        double Execute(double first, params double[] rest);
+    }
+}
