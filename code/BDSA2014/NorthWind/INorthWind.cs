d12f1cf583800972437d1aed39c22f41ce92d308
phillipphoelich <pphoelich@gmail.com>
2014-09-30 15:16:16 +0200

    Initial commit of NorthWind project and some interfaces.

---
+++ code/BDSA2014/NorthWind/INorthWind.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace NorthWind
+{
+    interface INorthWind
+    {
+        void AddOrder();
+        Product[] Products { get; }
+        Order[] Orders { get; } 
+    }
+}
