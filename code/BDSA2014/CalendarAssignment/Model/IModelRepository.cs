ba7ff420a81b2dee4e48a169e7ff2a5c20f39ad5
phillipphoelich <pphoelich@gmail.com>
2014-10-02 23:49:49 +0200

    Created all the interfaces and empty model classes for assignment 39!

---
+++ code/BDSA2014/CalendarAssignment/Model/IModelRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalendarAssignment.Model;
+
+namespace CalendarAssignment
+{
+    interface IModelRepository
+    {
+        void AddCalendar(Calendar calendar);
+        void AddEvent(Event givenEvent);
+        User AddUser(string username, string password);
+        void RemoveCalendar(Calendar calendar);
+        void RemoveEvent(Event givenEvent);
+        User RemoveUser(string username, string password);
+    }
+}
